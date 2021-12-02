4570c727eaf0a398e3064dac641a57dcbec605db
maurici0nunes <tecinfomauricio@gmail.com>
2021-12-02 08:17:56 -0300

    add projeto#3

---
+++ ReactJS/Projeto03/src/components/SubButton/index.tsx
@@ -0,0 +1,42 @@
+import { useSession, signIn } from "next-auth/client";
+import { useRouter } from "next/router";
+import { api } from "../../services/api";
+import { getStripeJs } from "../../services/stripe-js";
+import styles from "./styles.module.scss";
+
+interface SubButtonProps {
+  priceId: string;
+}
+
+export function SubButton({ priceId }: SubButtonProps) {
+  const [session] = useSession();
+  const router = useRouter();
+
+  async function handleSub() {
+    if (!session) {
+      signIn("github");
+      return;
+    }
+
+    if (session.activeSub) {
+      router.push("/posts");
+      return;
+    }
+
+    try {
+      const response = await api.post("/subscribe");
+      const { sessionId } = response.data;
+      const stripe = await getStripeJs();
+
+      await stripe.redirectToCheckout({ sessionId });
+    } catch (err) {
+      alert(err.message);
+    }
+  }
+
+  return (
+    <button type="button" className={styles.subButton} onClick={handleSub}>
+      Subscribe now
+    </button>
+  );
+}
---
+++ ReactJS/Projeto03/src/pages/api/_lib/manageSub.ts
@@ -0,0 +1,38 @@
+import { query as q } from "faunadb";
+import { fauna } from "../../../services/fauna";
+import { stripe } from "../../../services/stripe";
+
+export async function saveSub(
+  subId: string,
+  customerId: string,
+  createAction = false
+) {
+  const userRef = await fauna.query(
+    q.Select(
+      "ref",
+      q.Get(q.Match(q.Index("user_by_stripe_customer_id"), customerId))
+    )
+  );
+
+  const sub = await stripe.subscriptions.retrieve(subId);
+
+  const subData = {
+    id: sub.id,
+    userId: userRef,
+    status: sub.status,
+    priceId: sub.items.data[0].price.id,
+  };
+
+  if (createAction) {
+    await fauna.query(
+      q.Create(q.Collection("subscriptions"), { data: subData })
+    );
+  } else {
+    await fauna.query(
+      q.Replace(
+        q.Select("ref", q.Get(q.Match(q.Index("subscription_by_id"), subId))),
+        { data: subData }
+      )
+    );
+  }
+}
